05bcba577abc8f0e5f9c964896438baf7a2f7b7b
Menezes1518 <laryssa.18menezes@gmail.com>
2025-01-31 16:43:28 -0300

    HTTPS2

--- aula-12/http.js
+++ aula-12/http.js
@@ -2,14 +2,25 @@ const http = require('http')
 
 const server = http.createServer((request, response) => {
     const {method, statusCode, url } = request
+    const sports = ['soccer', 'volley', 'basketball', 'tennis']
 
     if (url === '/') {
          response.write('<div><h1> Hello from node</h1> <p>http server</p></div>')
-         response.end()
+         return;
       
     }
+
+    if (url === '/api/sports') {
+        response.write(JSON.stringify(sports))
+        response.end()
+    }
+
+    response.statusCode = 404 
+    response.write('<h1>Página não encontrada</h1>')
+    response.end()
 })
 
 server.listen(3000, 'localhost', () => {
     console.log('server runnig on http://localhost:3000')
+    
 })
